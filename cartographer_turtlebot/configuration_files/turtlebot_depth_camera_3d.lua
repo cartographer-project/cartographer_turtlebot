70b3b4c0368b4d58be3bd1e008708c8e372c2ba6
Wolfgang Hess <whess@google.com>
2017-06-30 11:16:12 +0200

    Remove option unused after googlecartographer/cartographer#375. (#60)

--- cartographer_turtlebot/configuration_files/turtlebot_depth_camera_3d.lua
+++ cartographer_turtlebot/configuration_files/turtlebot_depth_camera_3d.lua
@@ -55,7 +55,6 @@ TRAJECTORY_BUILDER_3D.ceres_scan_matcher.occupied_space_weight_1 = 15.
 TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 4.
 TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 1e3
 TRAJECTORY_BUILDER_3D.ceres_scan_matcher.only_optimize_yaw = true
-TRAJECTORY_BUILDER_3D.kalman_local_trajectory_builder.scan_matcher_variance = 1e-8
 
 SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 0.2
 SPARSE_POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
