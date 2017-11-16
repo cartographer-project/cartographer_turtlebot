acd372dea4841ccbcf33c4caa57fd367c2666fd2
Wolfgang Hess <whess@google.com>
2017-11-16 11:53:39 +0100

    Follow googlecartographer/cartographer#682.
    [RFC=0001](https://github.com/googlecartographer/rfcs/blob/master/text/0001-renaming-sparse-pose-graph.md)

--- cartographer_turtlebot/configuration_files/turtlebot_urg_lidar_2d.lua
+++ cartographer_turtlebot/configuration_files/turtlebot_urg_lidar_2d.lua
@@ -46,7 +46,7 @@ TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
-SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.65
-SPARSE_POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
 return options
